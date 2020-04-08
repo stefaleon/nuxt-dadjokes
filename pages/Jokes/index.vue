982ae68293bd30e52dbd5508d3af450fc2ce587e
stefaleon <stefaleon@gmail.com>
2020-04-08 18:56:24 +0300

    get jokes from api with axios

--- pages/Jokes/index.vue
+++ pages/Jokes/index.vue
@@ -3,8 +3,40 @@
 </template>
 
 <script>
-export default {};
+import axios from "axios";
+export default {
+  data() {
+    return {
+      jokes: []
+    };
+  },
+  async created() {
+    const config = {
+      headers: {
+        Accept: "application/json"
+      }
+    };
+
+    try {
+      const res = await axios.get("https://icanhazdadjoke.com/search", config);
+      console.log(res.data);
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  head() {
+    return {
+      title: "Dad Jokes",
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: "best place for corny dad jokes"
+        }
+      ]
+    };
+  }
+};
 </script>
 
-<style>
-</style>
+<style></style>
