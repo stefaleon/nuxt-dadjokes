f425de378f35cc176463fd45e41351263d89fcb3
stefaleon <stefaleon@gmail.com>
2020-04-08 14:26:37 +0300

    about page

---
+++ pages/about.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>About DadJokes</h1>
+    <p>App that displays dad jokez</p>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: "About the App",
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: "best place for corny dad jokes"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+</style>
